5c173195cded7b3761f9aa54dd9052c76d0fe523
UTF8 <utf8@UTF8-MacBook-Pro.local>
2016-12-31 03:19:27 +0300

    Initial Commit

---
+++ touchbarTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  touchbarTest
+//
+//  Created by UTF8 on 31.12.2016.
+//  Copyright © 2016 UTF8. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ touchbarTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  touchbarTest
+//
+//  Created by UTF8 on 31.12.2016.
+//  Copyright © 2016 UTF8. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
